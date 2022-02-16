48cb1d4fd0c5325135e3e14d31b074236a27fa7a
ppk <ppk_student@hotmail.com>
2022-02-16 11:36:07 +1000

    Add project files.

---
+++ WebApplication12/Controllers/Auth.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplication12.Auth;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication12.Controllers
+{
+    [Route("Auth")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "tested";
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Client loginPass)
+        {
+            // здесь проверка пароля в бд
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, loginPass.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, "студент")
+                };
+            ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                notBefore: DateTime.Now,
+                claims: claimsIdentity.Claims,
+                expires: DateTime.Now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = loginPass.Login
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ WebApplication12/Controllers/Client.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace WebApplication12.Controllers
+{
+    public class Client
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
